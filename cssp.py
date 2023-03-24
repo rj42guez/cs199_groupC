5c0ced8dd5d00bd9b0082a77ec1fa686e6a5fb91
Raphael Justin C. Portuguez <62928512+rj42guez@users.noreply.github.com>
2023-03-24 13:00:50 +0800

    Add files via upload

---
+++ cssp.py
@@ -0,0 +1,19 @@
+n = int(input(f'Enter n: '))
+
+sequences = []
+substrings = []
+
+m = 0
+
+for i in range (n):
+    sequences.append(input(f'Enter sequence {i+1}: '))
+
+
+
+L = int(input(f'Enter L: '))
+
+
+
+
+
+print(sequences)
